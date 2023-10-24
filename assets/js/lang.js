800f62d487c94e6694c9b10b645f5b566840f362
Antonio Ortiz Avila <aortiz.muchaweb@gmail.com>
2023-10-24 18:02:54 -0500

    implement base to internationalitation i18n

---
+++ assets/js/lang.js
@@ -0,0 +1,54 @@
+let translations = {};
+
+const baseWorld = 'phaino-'
+const nameDataOptionLang = 'data-default-i18n';
+const nameOptionLang = 'data-option-i18n';
+
+const optionLang = {
+    0: "es",
+    1: "en"
+}
+
+const fetchContent = async (optionLang) => {
+    const response = await fetch(`lang/${optionLang}.json`);
+    const data = await response.json();
+    translations = data;
+
+    if (translations) {
+        translatePage( translations);
+    }
+    
+};
+
+const translateMenu = (options) => {
+    Object.entries(options).forEach(([key, val]) => {
+        document.querySelector(`#${baseWorld}${key}`).textContent = val
+    })
+
+}
+
+const translatePage = ( data ) => {
+
+    if (Object.keys(data.menu).length > 0) {
+        translateMenu(data.menu)
+    }
+
+};
+
+
+const switcher = document.querySelector('#localization-switcher-phaino');
+
+switcher.addEventListener('click', (e) => {
+
+    const option = parseInt(e.target.getAttribute(nameOptionLang))
+    
+    e.target.setAttribute(nameDataOptionLang, optionLang[option]);
+    e.target.setAttribute(nameOptionLang, option === 0 ? 1 : 0);
+
+    const acronimLang = e.target.getAttribute(nameDataOptionLang)
+
+    if (acronimLang) {
+        fetchContent(acronimLang);
+    }
+    
+});
